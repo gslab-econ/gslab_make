8aea07593824592ce8019685f2ccbcedc65c99d2
Jacob Conway <jcconway@stanford.edu>
2018-09-19 18:30:48 -0700

    #9 added unit tests for makelog functions: start, end, and write

--- gslab_make_dev/tests/test_create_links.py
+++ gslab_make_dev/tests/test_create_links.py
@@ -1,80 +1,80 @@
-#! /usr/bin/env python
-import unittest, sys, os, shutil
-from gslab_make_dev.write_logs import start_makelog, end_makelog
-from gslab_make_dev.create_links import create_links
-from gslab_make_dev.dir_mod import clear_dir
-from nostderrout import nostderrout
-import time
+# #! /usr/bin/env python
+# import unittest, sys, os, shutil
+# from gslab_make_dev.write_logs import start_makelog, end_makelog
+# from gslab_make_dev.create_links import create_links
+# from gslab_make_dev.dir_mod import clear_dir
+# from nostderrout import nostderrout
+# import time
     
 
-class testCreateLinks(unittest.TestCase):
+# class testCreateLinks(unittest.TestCase):
 
-    def setUp(self):
-        with nostderrout():
-            clear_dir(['../input', '../log'])  
-            start_makelog()
+#     def setUp(self):
+#         with nostderrout():
+#             clear_dir(['../input', '../log'])  
+#             start_makelog()
 
-    def test_default(self):
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list.txt'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/lyx_test_file.lyx'))
-        with nostderrout():
-            create_links(['gslab_make_dev/tests/input/test_file_list.txt'])
-        time.sleep(.1)
-        self.assertTrue(os.path.isfile('../input/linked_lyx_file'))
-        self.assertTrue(os.path.isdir('../input/linked_folder'))
-        with nostderrout():
-            os.remove('../input/linked_lyx_file')
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/lyx_test_file.lyx'))
+#     def test_default(self):
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list.txt'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/lyx_test_file.lyx'))
+#         with nostderrout():
+#             create_links(['gslab_make_dev/tests/input/test_file_list.txt'])
+#         time.sleep(.1)
+#         self.assertTrue(os.path.isfile('../input/linked_lyx_file'))
+#         self.assertTrue(os.path.isdir('../input/linked_folder'))
+#         with nostderrout():
+#             os.remove('../input/linked_lyx_file')
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/lyx_test_file.lyx'))
 
-    def test_bad_list(self):
-        with nostderrout():
-            create_links('nonexistent_list')
-        self.assertIn('Error with create_links', open('../log/make.log').read())
+#     def test_bad_list(self):
+#         with nostderrout():
+#             create_links('nonexistent_list')
+#         self.assertIn('Error with create_links', open('../log/make.log').read())
 
-    def test_wildcard(self):
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait1.m'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait2.m'))
-        with nostderrout():
-            create_links('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt')
-        time.sleep(.1)
-        self.assertTrue(os.path.isfile('../input/matlab_link1.m'))
-        self.assertTrue(os.path.isfile('../input/matlab_link2.m'))
+#     def test_wildcard(self):
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait1.m'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait2.m'))
+#         with nostderrout():
+#             create_links('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt')
+#         time.sleep(.1)
+#         self.assertTrue(os.path.isfile('../input/matlab_link1.m'))
+#         self.assertTrue(os.path.isfile('../input/matlab_link2.m'))
 
-    def test_multiple_lists(self):
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list.txt'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait1.m'))
-        self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait2.m'))
-        with nostderrout():
-            create_links('gslab_make_dev/tests/input/test_file_list.txt', \
-                            'gslab_make_dev/tests/input/test_file_list_w_wildcard.txt')
-        time.sleep(.1)
-        self.assertTrue(os.path.isfile('../input/linked_lyx_file'))
-        self.assertTrue(os.path.isdir('../input/linked_folder'))
-        self.assertTrue(os.path.isfile('../input/matlab_link1.m'))
-        self.assertTrue(os.path.isfile('../input/matlab_link2.m'))
+#     def test_multiple_lists(self):
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list.txt'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/test_file_list_w_wildcard.txt'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait1.m'))
+#         self.assertTrue(os.path.isfile('gslab_make_dev/tests/input/matlab_test_script_wait2.m'))
+#         with nostderrout():
+#             create_links('gslab_make_dev/tests/input/test_file_list.txt', \
+#                             'gslab_make_dev/tests/input/test_file_list_w_wildcard.txt')
+#         time.sleep(.1)
+#         self.assertTrue(os.path.isfile('../input/linked_lyx_file'))
+#         self.assertTrue(os.path.isdir('../input/linked_folder'))
+#         self.assertTrue(os.path.isfile('../input/matlab_link1.m'))
+#         self.assertTrue(os.path.isfile('../input/matlab_link2.m'))
 
-    def test_change_log(self):
-        with nostderrout():
-            end_makelog()
-            newlog = '../input/new_log'
-            start_makelog(makelog = newlog)
-            create_links('nonexistent_list', makelog = newlog)
-        self.assertIn('Error with create_links', open(newlog).read())
+#     def test_change_log(self):
+#         with nostderrout():
+#             end_makelog()
+#             newlog = '../input/new_log'
+#             start_makelog(makelog = newlog)
+#             create_links('nonexistent_list', makelog = newlog)
+#         self.assertIn('Error with create_links', open(newlog).read())
 
-    def test_change_linkdir(self):
-        with nostderrout():
-            create_links(['gslab_make_dev/tests/input/test_file_list.txt'], link_dir='../log/')
-            time.sleep(.1)
-        self.assertTrue(os.path.isfile('../log/linked_lyx_file'))
+#     def test_change_linkdir(self):
+#         with nostderrout():
+#             create_links(['gslab_make_dev/tests/input/test_file_list.txt'], link_dir='../log/')
+#             time.sleep(.1)
+#         self.assertTrue(os.path.isfile('../log/linked_lyx_file'))
 
-    def tearDown(self):
-        if os.path.isdir('../input/'):
-           shutil.rmtree('../input/')
-        if os.path.isdir('../log/'):
-           shutil.rmtree('../log/')
+#     def tearDown(self):
+#         if os.path.isdir('../input/'):
+#            shutil.rmtree('../input/')
+#         if os.path.isdir('../log/'):
+#            shutil.rmtree('../log/')
 
-if __name__ == '__main__':
-    os.getcwd()
-    unittest.main()
+# if __name__ == '__main__':
+#     os.getcwd()
+#     unittest.main()
---
+++ gslab_make_dev/tests/test_makelog.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+import unittest, sys, os, shutil
+from gslab_make_dev.write_logs import start_makelog, end_makelog, write_to_makelog
+from gslab_make_dev.dir_mod import clear_dir
+from gslab_make_dev.tests.nostderrout import nostderrout
+from gslab_make_dev.private.utility import norm_path
+import gslab_make_dev.private.metadata as metadata
+import gslab_make_dev.private.messages as messages
+from gslab_make_dev.private.exceptionclasses import CritError
+
+    
+
+class testMakeLog(unittest.TestCase):
+
+    def setUp(self):
+        with nostderrout():
+            clear_dir(['../log'])
+
+    def test_default(self):
+        self.assertFalse(metadata.makelog_started)
+        with nostderrout():
+            start_makelog()
+        self.assertIn(norm_path('.'), open('../log/make.log').read())
+        self.assertTrue(metadata.makelog_started)
+        self.assertIn(messages.note_makelog_start, open('../log/make.log').read())
+        self.assertIn(messages.note_working_directory, open('../log/make.log').read())
+        self.assertNotIn("Testing write", open('../log/make.log').read())
+        with nostderrout():
+            write_to_makelog("Testing write")
+        self.assertIn("Testing write", open('../log/make.log').read())
+        self.assertNotIn(messages.note_makelog_end, open('../log/make.log').read())
+        with nostderrout():
+            end_makelog()
+        self.assertIn(messages.note_makelog_end, open('../log/make.log').read())
+
+    def test_newlog(self):
+        newlog = '../log/new_log.log'
+        self.assertFalse(metadata.makelog_started)
+        with nostderrout():
+            start_makelog(makelog=newlog)
+        self.assertIn(norm_path('.'), open(newlog).read())
+        self.assertTrue(metadata.makelog_started)
+        self.assertIn(messages.note_makelog_start, open(newlog).read())
+        self.assertIn(messages.note_working_directory, open(newlog).read())
+        self.assertNotIn("Testing write", open(newlog).read())
+        with nostderrout():
+            write_to_makelog("Testing write", makelog=newlog)
+        self.assertIn("Testing write", open(newlog).read())
+        self.assertNotIn(messages.note_makelog_end, open(newlog).read())
+        with nostderrout():
+            end_makelog(makelog=newlog)
+        self.assertIn(messages.note_makelog_end, open(newlog).read())
+        self.assertFalse(os.path.isfile('../log/make.log'))
+
+    def test_not_started(self):
+        self.assertFalse(metadata.makelog_started)
+        with self.assertRaises(CritError):
+            write_to_makelog("Testing write")
+        with self.assertRaises(CritError):
+            end_makelog()
+
+    def test_log_deleted(self):
+        self.assertFalse(metadata.makelog_started)
+        with nostderrout():
+            start_makelog()
+            os.remove('../log/make.log')
+        self.assertTrue(metadata.makelog_started)
+        with self.assertRaises(CritError):
+            write_to_makelog("Testing write")
+
+    def tearDown(self):
+        if os.path.isdir('../log/'):
+           shutil.rmtree('../log/')
+        metadata.makelog_started = False
+
+if __name__ == '__main__':
+    os.getcwd()
+    unittest.main()
