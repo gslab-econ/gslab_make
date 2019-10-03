11a300a88cffc6c7b16aafa1cf322c6a267acaa9
Zong Huang <40323298+z-y-huang@users.noreply.github.com>
2019-10-03 15:02:43 -0700

    PR for #19: Refactor tablefill (#21)
    * #19: Start refactoring tablefill
    
    *  #19: Fix LaTeX tablefill
    
    * #19: Combine regex for LyX and Latex
    
    * #19: Make LaTeX regex less greedy
    
    * #19: Clean up code, add documentation

--- gslab_make/tablefill.py
+++ gslab_make/tablefill.py
@@ -15,7 +15,6 @@ colorama.init()
 import gslab_make.private.messages as messages
 from gslab_make.private.exceptionclasses import CritError, ColoredError
 from gslab_make.private.utility import norm_path, format_message
-from gslab_make.write_logs import write_to_makelogs
 
 def parse_tag(tag):
     """ Parse tag from input."""
@@ -45,10 +44,12 @@ def parse_data(data, null):
         List of data values from input.
     """
     null_strings = ['', '.', 'NA']
-    
-    data = [row.strip().split('\t') for row in data]
+     
+    data = [row.rstrip('\r\n') for row in data]
+    data = [row for row in data if row]
+    data = [row.split('\t') for row in data]
     data = chain(*data)
-    data = [value.strip() for value in data]
+    data = [*data]
     if (null != None):
         data = [null if value in null_strings else value for value in data]
     
@@ -72,38 +73,70 @@ def parse_content(file, null):
     
     
 def insert_value(line, value, type):
+    """ Insert value into line.
+    
+    Parameters
+    ----------
+    line : str
+        Line of document to insert value.
+    value : str
+        Value to insert.
+    type : str
+        Formatting for value.
+
+    Returns
+    -------
+    line : str
+        Line of document with inserted value.
+    """
+    
     if (type == 'no change'):
-        line = line.replace('###', value)
+        line = re.replace('\\\\?#\\\\?#\\\\?#', value, line)
     elif (type == 'round'):
         try:
             value = float(value)
         except:
             raise_from(CritError(messages.crit_error_not_float % value), None)
 
-        digits = re.findall('#([0-9]+)#', line)[0]
+        digits = re.findall('\\\\?#([0-9]+)\\\\?#', line)[0]
         rounded_value = format(value, '.%sf' % digits)
-        line = re.sub('(.*)#[0-9]+#', r'\g<1>' + rounded_value, line)
+        line = re.sub('(.*?)\\\\?#[0-9]+\\\\?#', r'\g<1>' + rounded_value, line)
     elif (type == 'comma + round'):
         try:
             value = float(value)
         except:
             raise_from(CritError(messages.crit_error_not_float % value), None)
 
-        digits = re.findall('#([0-9]+),#', line)[0]
+        digits = re.findall('\\\\?#([0-9]+),\\\\?#', line)[0]
         rounded_value = format(value, ',.%sf' % digits)
-        line = re.sub('(.*)#[0-9]+,#', r'\g<1>' + rounded_value, line)
+        line = re.sub('(.*?)\\\\?#[0-9]+,\\\\?#', r'\g<1>' + rounded_value, line)
 
     return(line)
 
 
 def insert_tables_lyx(template, tables):
+    """ Fill tables for LyX template.
+    
+    Parameters
+    ----------
+    template : str
+        Path of LyX template to fill.
+    tables : dict
+        Dictionary {tag: values} of tables.
+
+    Returns
+    -------
+    template : str
+        Filled LyX template.
+    """
+
     with open(template, 'r') as f:
         doc = f.readlines()
       
     is_table = False
 
     for i in range(len(doc)):
-        if doc[i].startswith('name "tab:'):
+        if re.match('name "tab:', doc[i]):
             tag = doc[i].replace('name "tab:','').rstrip('"\n').lower()
             
             try:
@@ -133,7 +166,7 @@ def insert_tables_lyx(template, tables):
                         raise_from(CritError(messages.crit_error_too_many_values % tag), None)
                 else:
                     break
-            except KeyError:
+            except IndexError:
                 raise_from(CritError(messages.crit_error_not_enough_values % tag), None)
                 
 
@@ -142,15 +175,30 @@ def insert_tables_lyx(template, tables):
 
 
 def insert_tables_latex(template, tables):
+    """ Fill tables for LaTeX template.
+    
+    Parameters
+    ----------
+    template : str
+        Path of LaTeX template to fill.
+    tables : dict
+        Dictionary {tag: values} of tables.
+
+    Returns
+    -------
+    template : str
+        Filled LaTeX template.
+    """
+
     with open(template, 'r') as f:
         doc = f.readlines()
 
     is_table = False
 
     for i in range(len(doc)):
-        if doc[i].startswith('label{tab:'):
+        if re.search('label\{tab:', doc[i]):
             tag = doc[i].split(':')[1].rstrip('}\n').strip('"').lower()
-            
+
             try:
                 values = tables[tag]
                 entry_count = 0
@@ -161,34 +209,47 @@ def insert_tables_latex(template, tables):
         while is_table:
             line_col = doc[i].split("&")
 
-            for col in line_col:
-                try:
-                    if re.match('.*###', col):
-                        col = insert_value(col, values[entry_count], 'no change')
-                        entry_count += 1
-                        break
-                    elif re.match('.*#[0-9]+#', col):
-                        col = insert_value(col, values[entry_count], 'round')
-                        entry_count += 1
-                        break
-                    elif re.match('.*#[0-9]+,#', col):
-                        col = insert_value(col, values[entry_count], 'comma + round')
-                        entry_count += 1
-                        break
-                except KeyError:
-                    raise_from(CritError(messages.crit_error_not_enough_values % tag), None)
-                
+            for j in range(len(line_col)):
+                if re.search('.*\\\\#\\\\#\\\\#', line_col[j]):
+                    line_col[j] = insert_value(line_col[j], values[entry_count], 'no change')
+                    entry_count += 1
+                elif re.search('.*\\\\#[0-9]+\\\\#', line_col[j]):
+                    line_col[j] = insert_value(line_col[j], values[entry_count], 'round')                   
+                    entry_count += 1
+                elif re.search('.*\\\\#[0-9]+,\\\\#', line_col[j]):
+                    line_col[j] = insert_value(line_col[j], values[entry_count], 'comma + round')
+                    entry_count += 1
+               
             doc[i] = "&".join(line_col)
-                
-            if doc[i] == 'end{tabular}':
+
+            if re.search('end\{tabular\}', doc[i], flags = re.IGNORECASE):
                 is_table = False
                 if entry_count != len(values):
                     raise_from(CritError(messages.crit_error_too_many_values % tag), None)
             else:
-            	break
+                break
+
+    doc = '\n'.join(doc)
+
+    return(doc)
 
 
 def insert_tables(template, tables):
+    """ Fill tables for template.
+    
+    Parameters
+    ----------
+    template : str
+        Path of template to fill.
+    tables : dict
+        Dictionary {tag: values} of tables.
+
+    Returns
+    -------
+    template : str
+        Filled template.
+    """
+    
     if re.search('\.lyx', template):
         doc = insert_tables_lyx(template, tables)
     elif re.search('\.tex', template):
@@ -198,11 +259,31 @@ def insert_tables(template, tables):
 
 
 def tablefill(inputs, template, output, null = None):
+    """ Fill tables for template using inputs.
+    
+    Parameters
+    ----------
+    inputs : list
+        List of inputs to fill into template.
+    template : str
+        Path of template to fill.
+    output : str
+        Path of output.
+    null : str
+        Value to replace null characters (`''`, `'.'`, `'NA'`). Defaults to None.
+
+    Returns
+    -------
+    None
+    """
+
     try:
+        if type(inputs) is not list:
+            raise_from(TypeError(messages.type_error_dir_list % inputs), None)
         inputs = [norm_path(file) for file in inputs]
         content = [parse_content(file, null) for file in inputs]
         tables = {tag:data for (tag, data) in content}
-        
+
         if (len(content) != len(tables)):
             raise_from(CritError(messages.crit_error_duplicate_tables), None)
 
@@ -213,5 +294,4 @@ def tablefill(inputs, template, output, null = None):
     except:
         error_message = 'Error with `tablefill`. Traceback can be found below.' 
         error_message = format_message(error_message) 
-        write_to_makelog(paths, error_message + '\n\n' + traceback.format_exc())
         raise_from(ColoredError(error_message, traceback.format_exc()), None)
